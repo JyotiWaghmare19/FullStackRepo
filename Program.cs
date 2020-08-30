b93bff6dbf1a919eea3d5b03808067c61d4cf60e
Jyoti Waghmare <63786710+JyotiWaghmare19@users.noreply.github.com>
2020-08-30 19:16:28 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //Create stopwatch
+            Stopwatch Stopwatch = new Stopwatch();
+            
+            //Start timing
+            Stopwatch.start();
+
+            //Delay
+            Thread.Sleep(1); 
+
+            //Stop timing
+            Stopwatch.stop();
+           
+            Console.ReadKey();
+        }
+ 
+    }
+}
---
+++ Stopwatch.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    class Stopwatch
+    {
+        public DateTime starttime;
+        public DateTime stoptime;
+       
+        public bool isStarted;
+
+
+        public void stopwatch()
+        {
+            starttime = new DateTime();
+            stoptime = new DateTime();
+            isStarted = false;
+
+        }
+        public void start()
+        {
+            if (isStarted)
+                throw new InvalidOperationException();
+
+            starttime = DateTime.Now;
+            Console.WriteLine("Start Time: " + starttime.Hour + ":" + starttime.Minute + ":" + starttime.Second + ":" + starttime.Millisecond);
+        }
+        public void stop()
+        {
+
+            stoptime = DateTime.Now;
+            isStarted = false;
+            Console.WriteLine("Stop Time: " + stoptime.Hour + ":" + stoptime.Minute + ":" + stoptime.Second + ":" + stoptime.Millisecond);
+            TimeSpan duration = stoptime - starttime;
+        }
+       
+            private TimeSpan Interval;
+
+            public TimeSpan MyProperty
+            {
+               get { Interval = stoptime - starttime;
+                      return Interval;}
+             
+ 
+         }
+    }
+    
+}
